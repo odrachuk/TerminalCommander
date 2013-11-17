c5f1c0cc3a8fc90962cdcda5cd262c626b180aa0
Oleksand Drachuk <a.v.drachuk@gmail.com>
2013-11-17 15:47:40 +0200

    All problems with list was in click item listener

--- src/com/drk/terminal/ui/TerminalActivity.java
+++ src/com/drk/terminal/ui/TerminalActivity.java
@@ -1,13 +1,14 @@
 package com.drk.terminal.ui;
 
 import android.app.Activity;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.*;
+import android.view.*;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.ToggleButton;
 import com.drk.terminal.R;
 
 import java.util.ArrayList;
@@ -22,6 +23,33 @@ import java.util.concurrent.TimeUnit;
  * To change this template use File | Settings | File Templates.
  */
 public class TerminalActivity extends Activity {
+    private final CompoundButton.OnCheckedChangeListener mOnToggleListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (buttonView.getId() == R.id.action_shift) {
+                isShiftToggle = isChecked;
+                if (isChecked && mCtrlBtn.isChecked()) {
+                    mCtrlBtn.setChecked(false);
+                    isCtrlToggle = false;
+                }
+            } else if (buttonView.getId() == R.id.action_ctrl) {
+                isCtrlToggle = isChecked;
+                if (isChecked && mShiftBtn.isChecked()) {
+                    mShiftBtn.setChecked(false);
+                    isShiftToggle = false;
+                }
+            }
+        }
+    };
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int viewId = v.getId();
+            if (viewId == R.id.action_tab) {
+                //todo
+            }
+        }
+    };
     private boolean isShiftToggle, isCtrlToggle;
     private ToggleButton mShiftBtn, mCtrlBtn;
 
@@ -50,6 +78,39 @@ public class TerminalActivity extends Activity {
         valuesList.add(new DirectoryContentInfo("/Blackberry", "6540", "Jan 3 2013"));
         valuesList.add(new DirectoryContentInfo("/Windows7", "4322", "Nov 22 2011"));
         valuesList.add(new DirectoryContentInfo("/OS/2", "12345", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Windows7", "4322", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/OS/2", "12345", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Max OS X", "456", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Ubuntu", "765", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Linux", "8854", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Android", "50", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Blackberry", "876", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/OS/2", "234", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Max OS X", "8000", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Ubuntu", "234", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Linux", "23444", "Nov 22 2012"));
+        valuesList.add(new DirectoryContentInfo("/Windows7", "4322", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/OS/2", "12345", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Max OS X", "456", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Ubuntu", "765", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Linux", "8854", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Android", "50", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Blackberry", "876", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/OS/2", "234", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Max OS X", "8000", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Ubuntu", "234", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Linux", "23444", "Nov 22 2012"));
+        valuesList.add(new DirectoryContentInfo("/Windows7", "4322", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/OS/2", "12345", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Max OS X", "456", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Ubuntu", "765", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Linux", "8854", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Android", "50", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Blackberry", "876", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/OS/2", "234", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Max OS X", "8000", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Ubuntu", "234", "Nov 22 2011"));
+        valuesList.add(new DirectoryContentInfo("/Linux", "23444", "Nov 22 2012"));
         valuesList.add(new DirectoryContentInfo("/Max OS X", "456", "Nov 22 2011"));
         valuesList.add(new DirectoryContentInfo("/Ubuntu", "765", "Nov 22 2011"));
         valuesList.add(new DirectoryContentInfo("/Linux", "8854", "Nov 22 2011"));
@@ -61,16 +122,7 @@ public class TerminalActivity extends Activity {
         valuesList.add(new DirectoryContentInfo("/Linux", "23444", "Nov 22 2012"));
         final DirectoryContentAdapter adapter = new DirectoryContentAdapter(this, valuesList);
         listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-                final DirectoryContentInfo item = (DirectoryContentInfo) parent.getItemAtPosition(position);
-            }
-
-        });
+        listView.setOnTouchListener(new ListViewTouchListener(listView));
     }
 
     private void prepareRightList() {
@@ -84,16 +136,6 @@ public class TerminalActivity extends Activity {
         valuesList.add(new DirectoryContentInfo("/Linux", "23444", "Nov 22 2012"));
         final DirectoryContentAdapter adapter = new DirectoryContentAdapter(this, valuesList);
         listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-                final DirectoryContentInfo item = (DirectoryContentInfo) parent.getItemAtPosition(position);
-            }
-
-        });
     }
 
     @Override
@@ -154,32 +196,78 @@ public class TerminalActivity extends Activity {
         }
     }
 
-    private final CompoundButton.OnCheckedChangeListener mOnToggleListener = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if (buttonView.getId() == R.id.action_shift) {
-                isShiftToggle = isChecked;
-                if (isChecked && mCtrlBtn.isChecked()) {
-                    mCtrlBtn.setChecked(false);
-                    isCtrlToggle = false;
-                }
-            } else if (buttonView.getId() == R.id.action_ctrl) {
-                isCtrlToggle = isChecked;
-                if (isChecked && mShiftBtn.isChecked()) {
-                    mShiftBtn.setChecked(false);
-                    isShiftToggle = false;
-                }
-            }
-        }
-    };
+    private final class ListViewTouchListener implements View.OnTouchListener {
+        private ListView mListView;
+        private View mDownView;
+        private int mDownPosition;
 
-    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        private ListViewTouchListener(ListView listView) {
+            mListView = listView;
+        }
+
         @Override
-        public void onClick(View v) {
-            int viewId = v.getId();
-            if (viewId == R.id.action_tab) {
-                //todo
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    makeSelection(motionEvent);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    mDownView = null;
+                    mDownPosition = ListView.INVALID_POSITION;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    makeSelection(motionEvent);
+                    break;
+            }
+            return true;
+        }
+
+        private void makeSelection(MotionEvent motionEvent) {
+            // Find the child view that was touched (perform a hit test)
+            Rect rect = new Rect();
+            int childCount = mListView.getChildCount();
+            int[] listViewCoords = new int[2];
+            mListView.getLocationOnScreen(listViewCoords);
+            int x = (int) motionEvent.getRawX() - listViewCoords[0];
+            int y = (int) motionEvent.getRawY() - listViewCoords[1];
+            if (mDownView != null) {
+                mDownView.getHitRect(rect);
+                if (!rect.contains(x, y)) {
+                    View child;
+                    for (int i = 0; i < childCount; i++) {
+                        child = mListView.getChildAt(i);
+                        child.getHitRect(rect);
+                        if (rect.contains(x, y)) {
+                            mDownView = child;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                View child;
+                for (int i = 0; i < childCount; i++) {
+                    child = mListView.getChildAt(i);
+                    child.getHitRect(rect);
+                    if (rect.contains(x, y)) {
+                        mDownView = child;
+                        break;
+                    }
+                }
+            }
+
+            if (mDownView != null) {
+                int newPosition = mListView.getPositionForView(mDownView);
+                if (newPosition != mDownPosition) {
+                    mDownPosition = newPosition;
+                    mDownView.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
+                    for (int i = 0; i < childCount; i++) {
+                        if (mDownPosition != i) {
+                            View child = mListView.getChildAt(i);
+                            child.setBackgroundColor(getResources().getColor(R.color.COLOR_002EB8));
+                        }
+                    }
+                }
             }
         }
-    };
+    }
 }
