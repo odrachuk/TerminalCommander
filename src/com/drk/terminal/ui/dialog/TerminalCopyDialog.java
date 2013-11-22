9e6fdfd09883b01ca5460e955d8fe8b31ba81107
Oleksand Drachuk <a.v.drachuk@gmail.com>
2013-11-22 21:25:25 +0200

    Styled dialog

--- src/com/drk/terminal/ui/dialog/TerminalCopyDialog.java
+++ src/com/drk/terminal/ui/dialog/TerminalCopyDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import com.drk.terminal.R;
@@ -38,32 +39,14 @@ public class TerminalCopyDialog extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        mNum = savedInstanceState.get();
         setStyle(DialogFragment.STYLE_NO_TITLE, 0);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         View v = inflater.inflate(R.layout.terminal_copy_dialog_layout, container, false);
         return v;
     }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        EditText input = (EditText) view.findViewById(R.id.copy_dialog_destination_input);
-        input.clearFocus();
-        view.findViewById(R.id.terminal_copy_dialog_btn_ok).requestFocus();
-    }
-
-//    View.OnClickListener mOnClickListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            if (v.requestFocus()) {
-//                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-//                imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT);
-//            }
-//        }
-//    };
 }
