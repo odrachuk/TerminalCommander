4d677ecf828f000b4ca4763b27bc639f05b89e3c
Oleksand Drachuk <a.v.drachuk@gmail.com>
2013-12-05 20:29:35 +0200

    Some file formats was organized in one file

--- TerminalUtils/src/com/drk/terminal/util/utils/FileExtensions.java
+++ TerminalUtils/src/com/drk/terminal/util/utils/FileExtensions.java
@@ -36,7 +36,157 @@ public enum FileExtensions {
             map.put("zip", "Popular compression format");
             return map;
         }
-    };
+    },
+    VIDEO("Video") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("avi", "container (a shell, which enables any form of compression to be used)");
+            map.put("flv", "Flash video (encoded to run in a flash animation)");
+            map.put("mpeg", "multimedia container (most often used for Sony's PlayStation Portable and Apple's iPod)");
+            map.put("mp4", " multimedia container (most often used for Sony's PlayStation Portable and Apple's iPod)");
+            return map;
+        }
+    },
+    MUSIC("Music") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("bwf", "Broadcast Wave Format (BWF), an extension of WAVE");
+            map.put("wav", "Microsoft Wave");
+            map.put("flac", "(free lossless codec of the Ogg project)");
+            map.put("la", "Lossless Audio (.la)");
+            map.put("pac", "LPAC (.pac)");
+            map.put("ape", "Monkey's Audio (APE)");
+            map.put("mp2", "MPEG Layer 2");
+            map.put("mp3", "MPEG Layer 3");
+            map.put("wma", "Windows Media Audio (.WMA)");
+            map.put("aac", "Advanced Audio Coding (usually in an MPEG-4 container)");
+            map.put("mp4", "Advanced Audio Coding (usually in an MPEG-4 container)");
+            map.put("mpc", "Musepack");
+            return map;
+        }
+    },
+    WEB("Webpage") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("xml", "eXtensible Markup Language");
+            map.put("html", "HyperText Markup Language");
+            map.put("xhtml", "eXtensible HyperText Markup Language");
+            map.put("mhtml", "Archived HTML, store all data on one web page (text, images, etc.) in one big file");
+            map.put("jsp", "JavaServer Pages");
+            return map;
+        }
+    },
+    COMPUTER_PROGRAMS("Computer programs") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("ada", "Ada (body) source");
+            map.put("ads", "Ada (specification) source");
+            map.put("asm", "Assembly language source");
+            map.put("bas", "BASIC, FreeBASIC, Visual Basic, BASIC-PLUS source,[2] PICAXE basic");
+            map.put("bb", "Blitz3D");
+            map.put("bmx", "BlitzMax");
+            map.put("c", "C source");
+            map.put("clj", "Clojure source code");
+            map.put("cls", "Visual Basic class");
+            map.put("cob", "COBOL source");
+            map.put("cbl", "COBOL source");
+            map.put("cpp", "C++ source");
+            map.put("cc", "C++ source");
+            map.put("cxx", "C++ source");
+            map.put("cbp", "C++ source");
+            map.put("cs", "C# source");
+            map.put("csproj", "C# project (Visual Studio .NET)");
+            map.put("d", "D source");
+            map.put("dba", "DarkBASIC source");
+            map.put("dbpro123", "DarkBASIC Professional project");
+            map.put("e", "Eiffel source");
+            map.put("efs", "EGT Forever Source File");
+            map.put("egt", "EGT Asterisk Source File, could be J, C#, VB.net, EF 2.0 (EGT Forever)");
+            map.put("el", "Emacs Lisp source");
+            map.put("for", "Fortran source");
+            map.put("frm", "Visual Basic form");
+            map.put("frx", "Visual Basic form stash file (binary form file)");
+            map.put("fth", "Forth source");
+            map.put("ged", "Game Maker Extension Editable file as of version 7.0");
+            map.put("gm6", "Game Maker Editable file as of version 6.x");
+            map.put("gmd", "Game Maker Editable file up to version 5.x");
+            map.put("gmk", "Game Maker Editable file as of version 7.0");
+            map.put("gml", "Game Maker Language script file");
+            map.put("go", "Go source");
+            map.put("h", "C/C++ header file");
+            map.put("hpp", "C++ header file");
+            map.put("hxx", "C++ header file");
+            map.put("hs", "Haskell source");
+            map.put("i", "SWIG interface file");
+            map.put("inc", "Turbo Pascal included source");
+            map.put("java", "Java source");
+            map.put("l", "lex source");
+            map.put("lgt", "Logtalk source");
+            map.put("lisp", "Common Lisp source");
+            map.put("m", "Objective-C source");
+            map.put("pas", "Pascal source (DPR for projects)");
+            map.put("php", "PHP source");
+            map.put("php3", "PHP source");
+            map.put("php4", "PHP source");
+            map.put("php5", "PHP source");
+            map.put("phps", "PHP source");
+            map.put("pl", "Perl");
+            map.put("py", "Python source");
+            map.put("r", "R source");
+            map.put("rb", "Ruby source");
+            map.put("resx", "Resource file for .NET applications");
+            map.put("vb", "Visual Basic.NET source");
+            return map;
+        }
+    },
+    IMAGES("Raster graphics") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("bmp", "Microsoft Windows Bitmap formatted image");
+            map.put("gif", "CompuServe's Graphics Interchange Format");
+            map.put("ico", "a file format used for icons in Microsoft Windows. Contains small bitmap images at multiple resolutions and sizes.");
+            map.put("jpeg", "Joint Photographic Experts Group – a lossy image format widely used to display photographic images.");
+            map.put("jpj", "Joint Photographic Experts Group – a lossy image format widely used to display photographic images.");
+            map.put("mng", "Multiple Network Graphics, the animated version of PNG");
+            map.put("png", "Portable Network Graphic (lossless, recommended for display and edition of graphic images)");
+            map.put("psd", "Adobe Photoshop Drawing");
+            map.put("pdd", "Adobe Photoshop Drawing");
+            map.put("pxm", "Pixelmator image file");
+            map.put("tiff", "Tagged Image File Format (usually lossless, but many variants exist, including lossy ones)");
+            map.put("xcf", "GIMP image (from Gimp's origin at the eXperimental Computing Facility of the University of California)");
+            return map;
+        }
+    },
+    OFFICE_DOCUMENT("Office format") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("doc", "Microsoft Word document");
+            map.put("docx", "Office Open XML document");
+            map.put("dot", "Microsoft Word document template");
+            map.put("dotx", "Office Open XML text document template");
+            map.put("rtf", "Rich Text document");
+            map.put("txt", "ASCII nebo Unicode plaintext Text file");
+            map.put("info", "Texinfo");
+            return map;
+        }
+    },
+    BOOK_DOCUNET("Books format") {
+        @Override
+        public Map<String, String> categoryExtensions() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("fb2", "FB2 format");
+            map.put("pdf", "Portable Document Format");
+            map.put("djvu", "DjVu for scanned documents");
+            return map;
+        }
+    }
+    ;
     String description;
 
     private FileExtensions(String description) {
